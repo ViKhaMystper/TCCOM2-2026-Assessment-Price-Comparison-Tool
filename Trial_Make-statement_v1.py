47cd80ee1c780bdcb15cd0a8f56cbe83de2d13a4
ViKhaMystper <tranb4547@masseyhigh.school.nz>
2026-06-24 14:25:52 +1200

    Statement generator trial 1
    Simple 'make statement' function, one line only with emoji at the start and end.

---
+++ Trial_Make-statement_v1.py
@@ -0,0 +1,7 @@
+# Function go here
+def make_statement(statement, decoration):
+    """Emphasizes headings by adding decoration at the start and end"""
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+# main routine goes here
+make_statement("Price Comparison Tool", "💰")
