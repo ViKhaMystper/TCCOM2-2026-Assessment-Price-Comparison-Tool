1d5818cbfb67ed2471188b91085d631970e25d0c
ViKhaMystper <tranb4547@masseyhigh.school.nz>
2026-06-24 14:36:51 +1200

    weight checker trial 2
    Similar to trial 1 but user can enter more different units - mg, L, ml.

---
+++ Trial_weight_checker_v2.py
@@ -0,0 +1,47 @@
+# Function goes here
+def weight_check(question):
+    """Checks weight and converts to kg"""
+
+    while True:
+        error = "Please enter the weight of the item that more than 0, including units."
+        response = input(question).lower()
+
+        try:
+            # kilograms
+            if response[-2:] == "kg":
+                weight= float(response[:-2])
+
+            # grams
+            elif response[-1:] == "g":
+                weight= float(response[:-1]) / 1000
+
+            # milligrams
+            elif response[-2:] == "mg":
+                weight= float(response[:-2]) / 1000000
+
+
+                # millilitres
+            elif response[-2:] == "ml":
+                weight = float(response[:-2]) / 1000
+
+                # litres
+            elif response[-1:] == "l":
+                weight = float(response[:-1])
+
+            else:
+                print(error)
+                continue
+
+            if weight > 0:
+                return weight
+
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+# Main routine
+# Get item's weight
+item_weight = weight_check("Item weight: ")
+print(f"You entered {item_weight} kg")
